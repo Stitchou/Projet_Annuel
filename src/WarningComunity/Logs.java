2883f077674b1643f035dac20580989f9cb5eae6
Stitchou <la_muerte_97216@hotmail.com>
2014-04-01 18:40:13 +0200

    Fonction écrire log
    réalisation la fonction d'écriture de fichiers

--- src/WarningComunity/Logs.java
+++ src/WarningComunity/Logs.java
@@ -1,5 +1,30 @@
 package WarningComunity;
 
+import java.io.FileReader ;
+import java.io.FileWriter ;
+import java.io.BufferedReader ;
+import java.io.IOException ;
+import java.io.PrintWriter;
+
 public class Logs {
 
+	public static void ecrire(String path, String text) 
+	{
+		PrintWriter ecri ;
+		try
+		{
+			ecri = new PrintWriter(new FileWriter(path),true);
+			ecri.print(text);
+			ecri.flush();
+			ecri.close();
+		}
+		catch (NullPointerException a)
+		{
+			System.out.println("Erreur : pointeur null");
+		}
+		catch (IOException a)
+		{
+			System.out.println("Problème d'IO : "+ a.getMessage()+" "+path+" "+text);
+		}
+	}
 }
