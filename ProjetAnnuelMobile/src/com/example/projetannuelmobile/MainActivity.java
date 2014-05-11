6bcc05d2205207692dbd13dfd6b91209ae1767b3
arnauve <yehouda_arnauve@hotmail.com>
2014-05-11 16:47:49 +0200

    Projet mobile
    erreur au passage de MainActivity a Second

--- ProjetAnnuelMobile/src/com/example/projetannuelmobile/MainActivity.java
+++ ProjetAnnuelMobile/src/com/example/projetannuelmobile/MainActivity.java
@@ -1,35 +1,28 @@
 package com.example.projetannuelmobile;
 
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.location.Location;
+import java.sql.ResultSet;
+
+import com.example.projetannuelmobile.Second;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	long start;
-	//LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE); 
-	//Location l = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-	double longitude;
-	double latitude;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button b1 = (Button)findViewById(R.id.button1);
-		Button b2 = (Button)findViewById(R.id.button2);
-		Button b3 = (Button)findViewById(R.id.button3);
-		b1.setOnClickListener(this);
-		b2.setOnClickListener(this);
-		b3.setOnClickListener(this);
+		Button connect = (Button)findViewById(R.id.connection);
+		connect.setOnClickListener(this);
 	}
 
 	@Override
@@ -39,46 +32,28 @@ public class MainActivity extends Activity implements OnClickListener {
 		return true;
 	}
 
-
-	LocationManager  lmo  = (LocationManager)getSystemService(Context.LOCATION_SERVICE) ;
-	LocationListener llo  = new LocationManagerHelper(){
-		@Override
-		public void onLocationChanged(Location lc) {
-			super.onLocationChanged(lc) ; // On execute la méthode parente
-
-			latitude = LocationManagerHelper.getLatitude(); 
-			longitude = LocationManagerHelper.getLongitude(); 
-		}
-	};
+	
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		Toast t;
-		switch (v.getId())
-		{
-			case R.id.button1:
-				// instruction si on click sur le 1er bouton
-				t = Toast.makeText(this, " 1er BOUTON \nLatitude : " + latitude + "\nLongitude : " + longitude, Toast.LENGTH_LONG);
-				//t = Toast.makeText(this, " 1er BOUTON ", Toast.LENGTH_LONG);
+		EditText et1 = (EditText) findViewById(R.id.editText1);
+		EditText et2 = (EditText) findViewById(R.id.editText2);
+		
+		Editable ed1 = et1.getText();
+		Editable ed2= et2.getText();
+
+		String login = ed1.toString();
+		String log, pwd;//, mail;
+		String mdp = ed2.toString();
+		Toast t = null;
+		t = Toast.makeText(this,"Erreur dans le login ou le mot de passe !", Toast.LENGTH_LONG );
+		Intent i = new Intent(this, Second.class);
+		//if(v.getId() == R.id.connection) {
+		switch(v.getId()) {
+			case R.id.connection:
 				t.show();
-				break;
-			case R.id.button2:
-				// instruction si on click sur le 2eme bouton
-				t = Toast.makeText(this, " 2eme BOUTON \nLatitude : " + latitude + "\nLongitude : " + longitude, Toast.LENGTH_LONG);
-				//t = Toast.makeText(this, " 2eme BOUTON ", Toast.LENGTH_LONG);
-				//longitude = location.getLongitude();
-				//latitude = location.getLatitude();
-				t.show();
-				break;
-			case R.id.button3:
-				// instruction si on click sur le 3eme bouton
-				t = Toast.makeText(this, " 3eme BOUTON \nLatitude : " + latitude + "\nLongitude : " + longitude, Toast.LENGTH_LONG);
-				//t = Toast.makeText(this, " 3eme BOUTON ", Toast.LENGTH_LONG);
-				//longitude = location.getLongitude();
-				//latitude = location.getLatitude();
-				t.show();
+				i.putExtra("mode", 1);
+				startActivity(i);
 				break;
 		}
 	}
-
 }
